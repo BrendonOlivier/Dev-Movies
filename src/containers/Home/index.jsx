5255ed361700b4fdf4db2c19b9c52069c2f16f91
BrendonOlivier <brendonolivier@gmail.com>
2024-07-22 19:37:30 -0300

    Melhorando código, passando as funções de busca da API para um arquivo só

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import api from "../../services/api"
 
 import { Background, Info, Poster, Container, ContainerButtons } from './styles'
 import Button from '../../components/Button';
 import Slider from '../../components/Slider';
 import { getImages } from '../../utils/getImagens';
 import Modal from '../../components/Modal';
+import { getMovies, getSeries, getTopMovies, getTopPeoples, getTopSeries } from '../../services/getData';
 
 
 function Home() {
@@ -20,48 +20,16 @@ function Home() {
 
 
     useEffect(() => {
-        async function getMovies() {
-            const { data: { results } } = await api.get('/movie/popular')
+        async function getAllData() {
 
-            setMovie(results[0])
+            setMovie(await getMovies())
+            setTopMovies(await getTopMovies())
+            setTopSeries(await getTopSeries())
+            setPopularSeries(await getSeries())
+            setTopPeople(await getTopPeoples())
         }
 
-        async function getTopMovies() {
-            const { data: { results } } = await api.get('/movie/top_rated')
-
-
-            setTopMovies(results)
-        }
-
-        async function getTopSeries() {
-            const { data: { results } } = await api.get('/tv/top_rated')
-
-
-            console.log(results)
-            setTopSeries(results)
-        }
-
-        async function getSeries() {
-            const { data: { results } } = await api.get('/tv/popular')
-
-
-            console.log(results)
-            setPopularSeries(results)
-        }
-
-        async function getTopPeoples() {
-            const { data: { results } } = await api.get('/person/popular')
-
-
-            console.log(results)
-            setTopPeople(results)
-        }
-
-        getMovies();
-        getTopMovies();
-        getTopSeries();
-        getSeries();
-        getTopPeoples();
+        getAllData();
     }, [])
 
 
---
+++ src/services/getData.js
@@ -0,0 +1,43 @@
+import api from './api';
+
+// Buscando um filme Popular pela posição desejada
+export async function getMovies() {
+    const { data: { results } } = await api.get('/movie/popular')
+
+    return results[0]
+}
+
+// Busca os Top Filmes do momento
+export async function getTopMovies() {
+    const { data: { results } } = await api.get('/movie/top_rated')
+
+    return results
+}
+
+// Busca as Top Series do momento
+export async function getTopSeries() {
+    const { data: { results } } = await api.get('/tv/top_rated')
+
+    return results
+}
+
+// Busca as Series Populares de TV
+export async function getSeries() {
+    const { data: { results } } = await api.get('/tv/popular')
+
+    return results
+}
+
+// Buscando os top Artistas
+export async function getTopPeoples() {
+    const { data: { results } } = await api.get('/person/popular')
+
+    return results
+}
+
+// Buscando o filme pelo ID
+export async function getMovie(movieId) {
+    const { data: { results } } = await api.get(`/movie/${movieId}/videos`)
+
+    return results[1]
+}
