9bc0421096638f56ee02c2326a002787d8dd0306
BrendonOlivier <brendonolivier@gmail.com>
2024-07-22 18:22:29 -0300

    Criando e estilizando nosso Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import Logo from '../../assets/logo.png'
+import { Container, Menu, Li } from './styles'
+import { Link, useLocation } from 'react-router-dom'
 
+// com o 'useLocation' vou saber em que página eu estou, para personalizar os Links do meu Header
+// pego o 'pathname' e passo pra saber se e o link está ativo
 function Header() {
+    const [changeBackground, setChangeBackground] = useState(false);
+    const { pathname } = useLocation();
+
+    window.onscroll = () => {
+        if (!changeBackground && window.pageYOffset > 150) {
+            setChangeBackground(true)
+        }
+        if(changeBackground &&  window.pageYOffset <= 150 ) {
+            setChangeBackground(false)
+        }
+    }
 
     return (
-        <div>
-            <img src={Logo} alt="logo-dev-movie" style={{width: 500}} />
-        </div>
+        <Container changeBackground={changeBackground}>
+            <img src={Logo} alt="logo-dev-movie" />
+            <Menu>
+                <Li isActive={pathname === '/'}>
+                    <Link to="/">Home</Link>
+                </Li>
+                <Li isActive={pathname.includes('filmes')}>
+                    <Link to="/filmes">Filmes</Link>
+                </Li>
+                <Li isActive={pathname.includes('series')}>
+                    <Link to="/series">Séries</Link>
+                </Li>
+            </Menu>
+        </Container>
     )
 }
 
