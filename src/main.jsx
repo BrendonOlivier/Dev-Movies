940b4d8ed3c2f964ff6800078671274d4df03df2
BrendonOlivier <brendonolivier@gmail.com>
2024-07-22 14:47:20 -0300

    Style-components, Fontes e Estilos globais

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import GlobalStyle from './styles/globalStyles.js'
 import Router from './routes/routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+    <GlobalStyle />
   </React.StrictMode>
 )
